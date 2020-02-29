9adf43513dd4d73fbaf6a83ff7f4d2d5c712fbee
Jul10-S <julioserrata@protonmail.ch>
2020-02-29 15:45:02 +0100

    Fixed some operation problems

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,51 +1,54 @@
-const numPressed = document.querySelectorAll("#numP");
-const textDiv = document.querySelector(".screen-text");
-const prevText = document.querySelector(".prev-text");
+const numPressed = document.querySelectorAll("#pressedButton");
+const textDiv = document.querySelector("#screen-text");
+const prevText = document.querySelector("#prev-text");
 
 let buffer0 = "";
 let bufferS = "";
-let prevOperator = "";
-let nextOperator = "";
+let currentOperator = "";
+let storedResult = "";
 
-
-function handleMath(usedOperator) {
-    switch (usedOperator) {
-        case "+": 
-            textDiv.innerText = parseInt(bufferS) + parseInt(buffer0);
-            return parseInt(bufferS) + parseInt(buffer0)
+function handleMath(currentOperator) {
+    switch (currentOperator) {
+        case "+":    
+            if (storedResult != "") {
+                storedResult += parseInt(buffer0);
+                console.log(storedResult);
+            } else {
+                storedResult = parseInt(bufferS) + parseInt(buffer0);
+                console.log(storedResult);
+            }
+            textDiv.innerText = storedResult;
             break;
         case "-":
-            textDiv.innerText = parseInt(bufferS) - parseInt(buffer0);
-            return parseInt(bufferS) - parseInt(buffer0)
+            if (storedResult != "") {
+                storedResult -= parseInt(buffer0);
+                console.log(storedResult);
+            } else {
+                storedResult = parseInt(bufferS) - parseInt(buffer0);
+                console.log(storedResult);
+            }
+            textDiv.innerText = storedResult;
             break;
         case "*":
-            textDiv.innerText = parseInt(bufferS) * parseInt(buffer0);
-            return parseInt(bufferS) * parseInt(buffer0)
+            if (storedResult != "") {
+                storedResult *= parseInt(buffer0);
+                console.log(storedResult);
+            } else {
+                storedResult = parseInt(bufferS) * parseInt(buffer0);
+                console.log(storedResult);
+            }
+            textDiv.innerText = storedResult;
             break;
         case "/":
-            textDiv.innerText = parseInt(bufferS) / parseInt(buffer0);
-            return parseInt(bufferS) / parseInt(buffer0)
+            if (storedResult != "") {
+                storedResult /= parseInt(buffer0);
+                console.log(storedResult);
+            } else {
+                storedResult = parseInt(bufferS) / parseInt(buffer0);
+                console.log(storedResult);
+            }
+            textDiv.innerText = storedResult;
             break;
-    } 
-}
-
-function handleSymbol(valueT) {
-    if (valueT == "+" || valueT == "-" || valueT == "*" || valueT == "/" || valueT == "pi" || valueT == "log"  || valueT == "pow" || valueT == "sqrt") {   
-        if (bufferS != "") {
-            nextOperator += valueT;             
-            bufferS = handleMath(prevOperator);
-            buffer0 = "";
-            prevText.innerText = "";
-            textDiv.innerText = "";
-        } else {
-            bufferS +=  buffer0;
-            prevOperator += valueT;
-            buffer0 = "";
-            textDiv.innerText = "";
-        }
-    } else {
-        textDiv.innerText += valueT;
-        buffer0 += valueT;
     }
 }
 
@@ -53,29 +56,40 @@ for (let i = 0; i < numPressed.length; i++) {
     let toPrint = numPressed[i];
     toPrint.addEventListener('click', function() {
         if (toPrint.value != "<-" && toPrint.value != "C") {
-            if (toPrint.value != "=") {
-                handleSymbol(toPrint.value);
-            } else {
-                prevText.innerText = `${bufferS} ${prevOperator} ${buffer0}`;
-                if (nextOperator != "") {
-                    handleMath(nextOperator);
-                    prevText.innerText = `${bufferS} ${nextOperator} ${buffer0}`;
+            if (!Number(toPrint.value) && toPrint.value != "=") {
+                if (currentOperator != "") {
+                    handleMath(currentOperator);
+                    textDiv.innerText += toPrint.value;
+                    prevText.innerText = textDiv.innerText;
+                    buffer0 = "";
                 } else {
-                    handleMath(prevOperator);
-                    prevText.innerText = `${bufferS} ${prevOperator} ${buffer0}`;
-                }
-                 
+                    textDiv.innerText += toPrint.value;
+                    prevText.innerText = textDiv.innerText;
+                    currentOperator = toPrint.value;
+                    bufferS += buffer0;
+                    buffer0 = "";
+                } 
+            } else if (toPrint.value == "=") {
+                handleMath(currentOperator);
+                prevText.innerText = textDiv.innerText;
+                currentOperator = "";
+                buffer0 = "";
+            } else {
+                textDiv.innerText += toPrint.value;
+                buffer0 += parseInt(toPrint.value);
             }
-    } else if (toPrint.value == "C") {
-        textDiv.innerText = "";
-        prevText.innerText = "";
-        buffer0 = "";
-        bufferS = "";
-        prevOperator = "";
-        nextOperator = "";
-    } else if (toPrint.value == "<-") {
-        textDiv.innerText = "";
-        buffer0 = "";
-    }
-})
+            
+        } else if (toPrint.value == "C") {
+            textDiv.innerText = "";
+            prevText.innerText = "";
+            buffer0 = "";
+            bufferS = "";
+            storedResult = "";
+        } else if (toPrint.value == "<-") {
+            textDiv.innerText = "";
+            buffer0 = "";
+        }
+
+    })
 }
+ // TODO: 0-errors and repeated blocks
